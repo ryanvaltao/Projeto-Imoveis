6f3e8ec32ac9a25cb566e3a236dae4bd69754611
ryanvaltao <rv_moura@id.uff.br>
2026-04-10 16:59:30 -0300

    Script esqueleto

---
+++ imoveis-com-branchs.R
@@ -0,0 +1,18 @@
+#-=-=-= LEITURA DO BANCO DE DADOS =-=-=-
+ dados = read.csv("imoveis.csv", header = TRUE, sep = ";")
+ 
+#-=-=-= ESTRUTURA DO BANCO DE DADOS =-=-=-
+ str(dados)
+head(dados) 
+View(dados)
+
+#-=-=-= GRAFICOS =-=-=-
+
+#preencher na branch imoveis-graficos
+#usar histograma, boxplot, dispersao, barras e densidade
+
+#-=-=-= MEDIDAS =-=-=-
+
+#preencher na branch imoveis-medidas
+#usar comandos de calculo minimo, maximo, media, mediana,
+#e desvio padrao
